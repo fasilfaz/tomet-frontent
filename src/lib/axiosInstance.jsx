3419b3f5f2e18379ce3a81929d11739ff0b09ce5
Fasilfaz <fazsby2001@gmail.com>
2024-10-13 19:17:05 +0530

    url edit

--- src/lib/axiosInstance.jsx
+++ src/lib/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "https://ecommerce-server-voxn.onrender.com"
+    baseURL: "https://tomet-backent.vercel.app"
     // baseURL: "https://urban-nest-silk.vercel.app"
-    baseURL: "http://localhost:4000"
+    // baseURL: "http://localhost:4000"
 })
