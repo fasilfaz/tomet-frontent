f74473825930fe1c25de3b803bedf1b0a09a0fd2
Fasilfaz <fazsby2001@gmail.com>
2024-11-13 19:45:37 +0530

    base url changed

--- src/lib/axiosInstance.jsx
+++ src/lib/axiosInstance.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://tomet-backent-1.onrender.com"
+    // baseURL: "https://tomet-backent-1.onrender.com"
+    baseURL: "https://tomet-backent.vercel.app"
     // baseURL: "http://localhost:4000"
 })
